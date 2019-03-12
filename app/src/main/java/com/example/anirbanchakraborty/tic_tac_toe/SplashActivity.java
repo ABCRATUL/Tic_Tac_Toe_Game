24f5f417653a2ca20821d406c967f4ca7f7add88
AnirbanChakraborty <a.chakraborty58@yahoo.com>
2019-03-12 16:01:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/anirbanchakraborty/tic_tac_toe/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.anirbanchakraborty.tic_tac_toe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    public static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
---
+++ app/src/main/java/com/example/anirbanchakraborty/tic_tac_toe/highscores2p.java
@@ -0,0 +1,13 @@
+package com.example.anirbanchakraborty.tic_tac_toe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class highscores2p extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscores2p);
+    }
+}
